81c7488ae9e97cbbeedb60cb9c00e3bd044aa87c
AaronChelvan <aaron.chelvan@outlook.com>
2016-12-02 21:20:17 -0800

    Finished Day 3 Part 1

---
+++ day3part1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import re
+with open('day3part1_input.txt') as f: #'day3part1_input.txt' contains the input
+    lines = f.readlines()
+
+numPossibleTriangles = 0
+
+for line in lines:
+	numbers = line.split() #split the line across any whitespace 
+	
+	if int(numbers[0]) + int(numbers[1]) > int(numbers[2]) and \
+		int(numbers[1]) + int(numbers[2]) > int(numbers[0]) and \
+		int(numbers[0]) + int(numbers[2]) > int(numbers[1]):
+		numPossibleTriangles += 1
+
+print("Number of possible triangles = " + str(numPossibleTriangles))
