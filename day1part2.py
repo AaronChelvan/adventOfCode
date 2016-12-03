211516f2deeed9031e476d13d60230c798e4d4c8
AaronChelvan <aaron.chelvan@outlook.com>
2016-12-02 19:00:47 -0800

    Finished Day 1 Part 2

--- day1part2.py
+++ day1part2.py
@@ -33,7 +33,7 @@ def newDirection (oldDirection, move):
 inputString = "R1, R3, L2, L5, L2, L1, R3, L4, R2, L2, L4, R2, L1, R1, L2, R3, L1, L4, R2, L5, R3, R4, L1, R2, L1, R3, L4, R5, L4, L5, R5, L3, R2, L3, L3, R1, R3, L4, R2, R5, L4, R1, L1, L1, R5, L2, R1, L2, R188, L5, L3, R5, R1, L2, L4, R3, R5, L3, R3, R45, L4, R4, R72, R2, R3, L1, R1, L1, L1, R192, L1, L1, L1, L4, R1, L2, L5, L3, R5, L3, R3, L4, L3, R1, R4, L2, R2, R3, L5, R3, L1, R1, R4, L2, L3, R1, R3, L4, L3, L4, L2, L2, R1, R3, L5, L1, R4, R2, L4, L1, R3, R3, R1, L5, L2, R4, R4, R2, R1, R5, R5, L4, L1, R5, R3, R4, R5, R3, L1, L2, L4, R1, R4, R5, L2, L3, R4, L4, R2, L2, L4, L2, R5, R1, R4, R3, R5, L4, L4, L5, L5, R3, R4, L1, L3, R2, L2, R1, L3, L5, R5, R5, R3, L4, L2, R4, R5, R1, R4, L3"
 
 splitInput = re.split(', ', inputString)
-currentDirection = "U"
+currentDirection = "U" #Initially facing upwards
 currentLocation = [0,0] #[x,y] coordinates of where you currently are
 
 #A list of lists containing the coordinates of all locations that have been visited
@@ -49,7 +49,9 @@ for i in splitInput:
 
 	#Update visitedLocations
 	for j in range(distance):
-		newLocation = currentLocation
+
+		newLocation = currentLocation[:]
+
 		if currentDirection == "U":
 			newLocation[1] += j + 1
 		elif currentDirection == "D":
@@ -64,7 +66,9 @@ for i in splitInput:
 
 		for k in visitedLocations:
 			if (newLocation == k):
-				print("HQ is at " + str(newLocation))
+				print("HQ is at: " + str(newLocation))
+				distanceHQ = abs(newLocation[0]) + abs(newLocation[1])
+				print("Distance from HQ: " + str(distanceHQ))
 				exit() 
 		visitedLocations.append(newLocation)
 
